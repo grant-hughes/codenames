5157d16fcc55ae46b0dbfa66e6d92b9cb82c0f99
ghughes3 <grant.hughes@optum.com>
2019-10-14 15:41:04 -0400

    trying something

--- public/js/gameScript.js
+++ public/js/gameScript.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 
   const gameId = $('#gameId').text();
 
-  var socket = io("https://grant-hughes-app.herokuapp.com");
+  var socket = io();
 
   var selectedIds = [];
   var selectedClasses = [];
